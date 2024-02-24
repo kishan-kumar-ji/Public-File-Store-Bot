bb105db0edb83231f5f9168ccd211df26507a629
kishan-kumar-ji <158287711+kishan-kumar-ji@users.noreply.github.com>
2024-02-25 00:06:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 import os
 
-API_ID = int(os.environ.get("API_ID", '3393749'))
-API_HASH = os.environ.get("API_HASH", 'a15a5954a1db54952eebd08ea6c68b71')
-BOT_TOKEN = os.environ.get("BOT_TOKEN", '1892142872:AAFIkvJRdHmx_l3SAqr_Ol8AkIG_5EABgsE')
-DB_CHANNEL_ID = os.environ.get("DB_CHANNEL_ID", '-1001981909884')
+API_ID = int(os.environ.get("API_ID", '25269253'))
+API_HASH = os.environ.get("API_HASH", 'a4f351103f9cb0eb6cfed99770f648ad')
+BOT_TOKEN = os.environ.get("BOT_TOKEN", '6704331949:AAGpmMmvuXYsBijwSyGaNsrVa2F_1iu5J3M')
+DB_CHANNEL_ID = os.environ.get("DB_CHANNEL_ID", '-1002065381146')
 IS_PRIVATE = os.environ.get("IS_PRIVATE",False) # any input is ok But True preferable
-OWNER_ID = int(os.environ.get("OWNER_ID",'1061576483'))
+OWNER_ID = int(os.environ.get("OWNER_ID",'6669835291'))
 PROTECT_CONTENT = True
-UPDATE_CHANNEL = os.environ.get('UPDATE_CHANNEL', '')
+UPDATE_CHANNEL = os.environ.get('UPDATE_CHANNEL', '-1002055148028')
 AUTH_USERS = list(int(i) for i in os.environ.get("AUTH_USERS", "0").split(" ")) if os.environ.get("AUTH_USERS") else []
 if OWNER_ID not in AUTH_USERS:
     AUTH_USERS.append(OWNER_ID)
